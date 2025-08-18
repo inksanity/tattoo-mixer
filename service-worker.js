a04ca55480ede8965fddfc5b8ed15f801353e2f3
inksanity <36294213+inksanity@users.noreply.github.com>
2025-08-18 18:18:07 +1200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='tattoolab-v5c';
+const ASSETS=['./','./index.html','./app.css','./app.js','./manifest.json','./assets/mask_square.png','./assets/mask_splat.png','./icons/icon-64.png','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(m=>m||fetch(e.request).then(res=>{const copy=res.clone(); if(e.request.method==='GET' && (new URL(e.request.url)).origin===location.origin){caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));} return res;}).catch(()=>caches.match('./index.html'))))});
