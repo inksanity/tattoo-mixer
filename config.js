5a413b8ee4e24415d00384239ef43bca5c46538a
inksanity <36294213+inksanity@users.noreply.github.com>
2025-08-20 09:54:42 +1200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+<script>
+window.TL_CONFIG = {
+  proGate: true,
+  firebase: {
+    apiKey: "AIzaSyDPq3f1Vv0vP1OWJR5Cufogep71Wr_5bl0",
+    authDomain: "tattoo-lab.firebaseapp.com",
+    projectId: "tattoo-lab",
+    databaseURL: "https://tattoo-lab-default-rtdb.firebaseio.com"
+  }
+};
+</script>
