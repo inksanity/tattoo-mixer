f8d858624c3d6f55df9a380df4d5003dcde48818
inksanity <36294213+inksanity@users.noreply.github.com>
2025-08-20 08:38:56 +1200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='tattoolab-v5d7';
+const CACHE_NAME='tattoolab-v5d8';
 const ASSETS=['./','./index.html','./app.css','./app.js','./colorEngine.js','./manifest.json','./config.js','./assets/mask_square.png','./assets/mask_splat.png','./icons/icon-64.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))))});
